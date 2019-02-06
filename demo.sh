b03b582b6409d7f049e45bf81504d2216062197e
Michel Peterson <michel@redhat.com>
2019-02-06 13:29:47 +0200

    Add parameter '-s' to avoid setting up the cluster

--- demo.sh
+++ demo.sh
@@ -24,51 +24,67 @@ TYPE_SPEED=20
 #
 DEMO_PROMPT="${GREEN}➜ ${CYAN}\W ${RED}$(kubectl config current-context) ${WHITE}$ "
 
+# Defaults
+DO_SETUP=true
+
+# Handling of script CLI options
+unset OPTIND
+while getopts "s" opt; do
+    case $opt in
+        s)
+            DO_SETUP=false
+            ;;
+    esac
+done
+
 # hide the evidence
 clear
 
-if [[ ! -z $TYPE_SPEED ]]; then
-    SAVED_TYPE_SPEED=$TYPE_SPEED
-    unset TYPE_SPEED
-fi
-
-p "# Preparing demo..."
-
-# starting from scratch
-for vm in cluster1 cluster2; do
-    if [[ -d ~/.minikube/machines/${vm} ]]; then
-        p "# Existing ${vm}, deleting..."
-        pe "minikube delete -p ${vm}"
+if $DO_SETUP; then
+    if [[ ! -z $TYPE_SPEED ]]; then
+        SAVED_TYPE_SPEED=$TYPE_SPEED
+        unset TYPE_SPEED
     fi
 
-    p "# Creating ${vm}..."
-    p "minikube start -p ${vm}"
-    minikube start -p ${vm}|tee ${vm}_creation.log
+    p "# Preparing demo..."
 
-done
+    # starting from scratch
+    for vm in cluster1 cluster2; do
+        if [[ -d ~/.minikube/machines/${vm} ]]; then
+            p "# Existing ${vm}, deleting..."
+            pe "minikube delete -p ${vm}"
+        fi
 
-p "# Done preparing demo, press <enter> to continue..."
+        p "# Creating ${vm}..."
+        p "minikube start -p ${vm}"
+        minikube start -p ${vm}|tee ${vm}_creation.log
 
-wait
+    done
 
-if [[ ! -z $SAVED_TYPE_SPEED ]]; then
-    TYPE_SPEED=$SAVED_TYPE_SPEED
+    if [[ ! -z $SAVED_TYPE_SPEED ]]; then
+        TYPE_SPEED=$SAVED_TYPE_SPEED
+    fi
+
+    p "# Done preparing demo, press <enter> to continue..."
+    wait
 fi
 
 clear
 
 
-p "# Welcome to the federated demo"
+p "# Welcome to the federation demo"
 wait
 
 # Save script root directory
 SCRIPT_DIR=$(pwd)
 
-for vm in cluster1 cluster2; do
-    p "minikube start -p ${vm}"
-    cat ${vm}_creation.log
-    wait
-done
+if $DO_SETUP; then
+    for vm in cluster1 cluster2; do
+        p "minikube start -p ${vm}"
+        cat ${vm}_creation.log
+        wait
+    done
+fi
 
 ln -s $GOPATH/src/github.com/kubernetes-sigs/federation-v2/ src
 
@@ -77,8 +93,10 @@ wait
 
 pe "cd src"
 
-pe "./scripts/deploy-federation-latest.sh cluster2"
-wait
+if $DO_SETUP; then
+    pe "./scripts/deploy-federation-latest.sh cluster2"
+    wait
+fi
 
 # # show a prompt so as not to reveal our true nature after
 # # the demo has concluded
