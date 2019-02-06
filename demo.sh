862d027fc5e6dd141e48feba7a114fca1e7b7846
Michel Peterson <michel@redhat.com>
2019-02-06 11:03:50 +0200

    Add skeleton for the demo script

---
+++ demo.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+##################################
+# Configure the pre-import options
+##################################
+#
+# speed at which to simulate typing. bigger num = faster
+#
+TYPE_SPEED=20
+
+########################
+# include the magic
+########################
+. ../demo-magic.sh -n
+
+
+###################################
+# Configure the post-import options
+###################################
+#
+# custom prompt
+#
+# see http://www.tldp.org/HOWTO/Bash-Prompt-HOWTO/bash-prompt-escape-sequences.html for escape sequences
+#
+DEMO_PROMPT="${GREEN}➜ ${CYAN}\W ${RED}$(kubectl config current-context) ${WHITE}$ "
+
+# hide the evidence
+clear
